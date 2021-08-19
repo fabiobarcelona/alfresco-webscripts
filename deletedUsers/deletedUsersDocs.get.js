d27096bd6d5e1c603f7833fd2df74ca983da6fd5
Fabio Barcelona <43356571+fabiobarcelona@users.noreply.github.com>
2021-08-19 12:38:44 +0200

    Add files via upload

--- deletedUsers/deletedUsersDocs.get.js
+++ deletedUsers/deletedUsersDocs.get.js
@@ -6,7 +6,8 @@ var user=args.user?args.user:person.properties.userName;
 
 do
 {
-	var q = 'TYPE:\"cm:content\" AND ((+cm:creator:'+user+') OR (cm:owner:'+user+')) AND +cm:lockOwner:System AND @sys\\:node\\-dbid:['+fromNode+' TO MAX]';
+	//var q = 'TYPE:\"cm:content\" AND ((+cm:creator:'+user+' AND cm:owner IS NULL) OR (cm:owner:'+user+')) AND +cm:lockOwner:System AND @sys\\:node\\-dbid:['+fromNode+' TO MAX]';
+	var q = 'TYPE:"cm:content" AND ((cm:creator:'+user+' AND ISNULL:"cm:owner") OR (cm:creator:'+user+' AND -EXISTS:"cm:owner") OR (cm:owner:'+user+' AND cm:owner:"") OR (cm:owner:'+user+')) AND +cm:lockOwner:System AND @sys\\:node\\-dbid:['+fromNode+' TO MAX]';
 	
 	var sort1 = { column: '@sys:node-dbid', ascending: true };
 	var paging = { maxItems: pageSize, skipCount: 0 };
@@ -42,4 +43,11 @@ var nodes = search.query(s);
 var length = nodes.length;
 
 model.nodes = nodes;
-model.length = length;*/
+model.length = length;
+
+TYPE:"cm:content" AND cm:name:'1.png' AND ((cm:creator:NACEVMA) OR (cm:owner:NACEVMA)) AND +cm:lockOwner:System
+HEALYKE
+TYPE:'cm:content' AND cm:owner:WADECHR AND !cm:creator:WADECHR
+DMO checklist.docx
+
+TYPE:"cm:content" AND =cm:name:'DMO checklist.docx' AND ((cm:creator:HEALYKE AND ISNULL:'cm:owner') OR (cm:creator:HEALYKE AND -EXISTS:'cm:owner') OR (cm:owner:HEALYKE AND cm:owner:'') OR (cm:owner:HEALYKE))*/
