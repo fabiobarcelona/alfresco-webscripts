0194b64b5954f9c0c10c36559b9a790b07c015b3
Fabio Barcelona <43356571+fabiobarcelona@users.noreply.github.com>
2021-08-30 11:06:11 +0200

    Add files via upload

---
+++ deletedUsers2.0/deletedUsersDocs.get.html.ftl
@@ -0,0 +1,25 @@
+<input type="hidden" id="length" value="${length}">
+<input type="hidden" id="lastIndex" value="${lastIndex}">
+<input type="hidden" id="uniqueUsers" value="${uniqueUsers}">
+<#list docs as node>
+    <tr>
+		<td>${node.properties['cm:name']}</td>
+		<#if node.properties["cm:owner"]?exists><td>${node.properties['cm:owner']}</td>
+		<#else>
+			<td>${node.properties['cm:creator']}</td>
+        </#if>
+        <td>${node.displayPath?replace("/Company Home/","")}</td>
+        <td>${node.properties["cm:modified"]?date?replace("-"," ")}</td>
+        <#if node.properties["rt:endRetentionDate"]?exists><td>${node.properties["rt:endRetentionDate"]?date?replace("-"," ")}</td>
+		<#else>
+			<td>Unknown</td>
+		</#if>	
+        <#if node.properties["rt:status"]?exists><td>${node.properties["rt:status"]}</td>
+        <#else>
+			<td>Unknown</td>
+        </#if>
+        <td>
+			<a href='/share/page/document-details?nodeRef=${node.nodeRef}' target="_blank">Link</a>
+        </td>
+    </tr>
+</#list>
---
+++ deletedUsers2.0/deletedUsersDocs.get.js
@@ -0,0 +1,76 @@
+var pageSize = 1000; // no more than 1000!!!
+var nodes = null;
+var user = null;
+var queryRes = null;
+var fromNode = args.currentNode;
+var docs = [];
+var users = [];
+
+function onlyUnique(value, index, self)
+{
+	return self.indexOf(value) === index;
+}
+
+function checkUser(user)
+{
+	var s = {};
+	
+	s.page = {};
+	s.query = 'TYPE:"cm:person" AND =cm:userName:'+user;
+	s.language      = 'fts-alfresco';
+	s.page.maxItems = 1;
+	
+	queryRes = search.query(s);
+	
+	for each (res in queryRes)
+	{
+		if(res.properties["cm:email"] == null || res.properties["cm:email"].length == 0 )
+		{
+			return false
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
+
+var q = 'TYPE:"cm:content" AND +cm:lockOwner:System AND @sys\\:node\\-dbid:['+fromNode+' TO MAX]';
+
+var sort1 = { column: '@sys:node-dbid', ascending: true };
+var paging = { maxItems: pageSize, skipCount: 0 };
+var def = { query: q, store: 'workspace://SpacesStore', language: 'fts-alfresco', sort: [sort1], page: paging };
+
+nodes = search.query(def);	
+
+for each (node in nodes)
+{
+	if(node.properties["sys:node-dbid"] == fromNode)
+	{
+		continue;
+	}
+	
+	if (node.properties["cm:owner"] == null || node.properties["cm:owner"].length == 0)
+	{
+		user = node.properties["cm:creator"];
+	}
+	else
+	{
+		user = node.properties["cm:owner"];
+	}
+	
+	if(!checkUser(user))
+	{
+		users.push(user);
+		docs.push(node);
+	}
+}
+
+var length = docs.length;
+var lastIndex = nodes[nodes.length - 1].properties["sys:node-dbid"];
+var uniqueUsers = users.filter(onlyUnique).toString();
+
+model.uniqueUsers = uniqueUsers;
+model.lastIndex = lastIndex;
+model.docs = docs;
+model.length = length;
